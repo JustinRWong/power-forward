6343b056d70401853a0ff6eb808aa21f594ec549
JustinRWong <justinryanwong@berkeley.edu>
2021-04-02 01:43:34 -0700

    build and deploy sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 cd server
 
 gcloud builds submit --tag gcr.io/power-forward/flask-fire
+
+gcloud beta run deploy  core-flask-server --image gcr.io/power-forward/flask-fire --platform=managed --region=21 
