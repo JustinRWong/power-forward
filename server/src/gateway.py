77e78600ba36b183d3f31fa020e95314354bd2fc
JustinRWong <justinryanwong@berkeley.edu>
2021-04-07 12:34:14 -0700

    temporary placeholdder for google analytics

--- server/src/gateway.py
+++ server/src/gateway.py
@@ -59,6 +59,30 @@ def analytics(req):
         'username': "Power Forward App Analytics :)",
         'content': content_str
     }
+
+    send_to_discord(payload)
+    #
+    # google_analytics_data = {
+    #     'v': '1',  # API Version.
+    #     'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
+    #     # Anonymous Client Identifier. Ideally, this should be a UUID that
+    #     # is associated with particular user, device, or browser instance.
+    #     'cid': '555',
+    #     't': 'event',  # Event hit type.
+    #     'ec': category,  # Event category.
+    #     'ea': action,  # Event action.
+    #     'el': label,  # Event label.
+    #     'ev': value,  # Event value, must be an integer
+    # }
+    #
+    # response = requests.post(
+    #     'http://www.google-analytics.com/collect', data=data)
+    #
+    # # If the request fails, this will raise a RequestException. Depending
+    # # on your application's needs, this may be a non-error and can be caught
+    # # by the caller.
+    # response.raise_for_status()
+
     return send_to_discord(payload)
 
 def collect_suggestion(city, state, email):
